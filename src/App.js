159cecb93d9000b16a1dfe539ca5355a542531db
FlorMartinengo <fmartinen24@ucema.edu.ar>
2022-09-20 12:03:36 -0300

    fundamentos js 1

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+
+function App() {
+
+  const nombre = 'Florencia'
+  const apellido = "Martinengo"
+
+  console.log(nombre, apellido)
+  console.log('Hola', nombre)
+  
+  let edad = 19
+  
+  console.log(edad)
+  edad = 20
+  console.log(edad)
+
+  return (
+    <>
+      <h1>const - let - var</h1>
+    </>
+  );
+}
+
+export default App;
